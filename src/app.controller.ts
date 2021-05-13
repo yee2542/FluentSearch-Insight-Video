df3419fbbb75cefb6095430f168006c09d1da028
yee2542 <yee2542gtl@gmail.com>
2021-05-13 07:15:32 +0700

    add sample endpoint

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,24 @@
 import { Controller, Get } from '@nestjs/common';
+import path from 'path';
 import { AppService } from './app.service';
+import { VideoService } from './video/video.service';
+
+const VIDEO_PATH = path.resolve('/sample/sample-2.mp4');
 
 @Controller()
 export class AppController {
-  constructor(private readonly appService: AppService) {}
+  constructor(
+    private readonly appService: AppService,
+    private readonly videoService: VideoService,
+  ) {}
 
   @Get()
   getHello(): string {
     return this.appService.getHello();
+  }
+
+  @Get('/insight')
+  async getVideoInsight() {
+    return this.videoService.videoToFrames(VIDEO_PATH);
   }
 }
