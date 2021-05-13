3b3981de0a687da63cc1652bcaab9b407f297786
yee2542 <yee2542gtl@gmail.com>
2021-05-13 10:45:02 +0700

    add httmp provider to insight module

--- src/insight/insight.module.ts
+++ src/insight/insight.module.ts
@@ -1,7 +1,14 @@
-import { Module } from '@nestjs/common';
+import { HttpModule, Module } from '@nestjs/common';
 import { InsightService } from './insight.service';
 
+const MODEL_SERVICE_ENDPOINT = 'deepdetect:8080';
 @Module({
-  providers: [InsightService]
+  imports: [
+    HttpModule.register({
+      baseURL: `http://${MODEL_SERVICE_ENDPOINT}`,
+    }),
+  ],
+  providers: [InsightService],
+  exports: [HttpModule.register({}), InsightService],
 })
 export class InsightModule {}
