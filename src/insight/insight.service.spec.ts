6d895c2d5bb12530c5db98b0980ab1bf2fa07233
yee2542 <yee2542gtl@gmail.com>
2021-05-14 00:17:04 +0700

    fix bad unit test

--- src/insight/insight.service.spec.ts
+++ src/insight/insight.service.spec.ts
@@ -1,3 +1,4 @@
+import { HttpModule } from '@nestjs/common';
 import { Test, TestingModule } from '@nestjs/testing';
 import { InsightService } from './insight.service';
 
@@ -6,6 +7,7 @@ describe('InsightService', () => {
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
+      imports: [HttpModule],
       providers: [InsightService],
     }).compile();
 
