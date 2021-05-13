a85c0237d7e7540ffd8041c278f8b54257bc6957
yee2542 <yee2542gtl@gmail.com>
2021-05-13 09:00:52 +0700

    change sample file

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -3,7 +3,7 @@ import path from 'path';
 import { AppService } from './app.service';
 import { VideoService } from './video/video.service';
 
-const VIDEO_PATH = path.resolve('sample/sample-2.mp4');
+const VIDEO_PATH = path.resolve('sample/sample.mp4');
 
 @Controller()
 export class AppController {
