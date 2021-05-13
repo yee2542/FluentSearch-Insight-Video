c00a274013631743a4f93a540fbf2b118a71995a
yee2542 <yee2542gtl@gmail.com>
2021-05-13 08:52:00 +0700

    add insight example path

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -3,7 +3,7 @@ import path from 'path';
 import { AppService } from './app.service';
 import { VideoService } from './video/video.service';
 
-const VIDEO_PATH = path.resolve('/sample/sample-2.mp4');
+const VIDEO_PATH = path.resolve('sample/sample-2.mp4');
 
 @Controller()
 export class AppController {
@@ -19,6 +19,8 @@ export class AppController {
 
   @Get('/insight')
   async getVideoInsight() {
-    return this.videoService.videoToFrames(VIDEO_PATH);
+    console.log(VIDEO_PATH);
+    await this.videoService.videoToFrames(VIDEO_PATH);
+    return;
   }
 }
