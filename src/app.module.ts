0d865ad0fd6578874ca4ae007ca15143c08fecf6
yee2542 <yee2542gtl@gmail.com>
2021-05-30 05:14:41 +0700

    add mongoose module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,9 @@
 import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
 import { ServeStaticModule } from '@nestjs/serve-static';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ConfigDatabaseService } from './config/config.database.service';
 import { ConfigModule } from './config/config.module';
 import { InsightModule } from './insight/insight.module';
 import { VideoModule } from './video/video.module';
@@ -10,6 +12,10 @@ import { TMP_DIR_PATH } from './video/video.service';
 @Module({
   imports: [
     ConfigModule,
+    MongooseModule.forRootAsync({
+      imports: [ConfigModule],
+      useClass: ConfigDatabaseService,
+    }),
     ServeStaticModule.forRoot({
       rootPath: TMP_DIR_PATH,
       serveRoot: '/file',
